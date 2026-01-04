12afc68c7912b7e0640738c2c63488f543a412e9
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2026-01-04 16:08:57 +0100

    Correction du composant button-period

--- src/Controller/ComposantController.php
+++ src/Controller/ComposantController.php
@@ -6,10 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ComponentController extends AbstractController
+class ComposantController extends AbstractController
 {
 
-    #[Route('/composants/button-period', name: 'composants_button_period')]
+    #[Route('/composants/button-period', name: 'composants_button_period', methods: ['GET'])]
     public function buttonPeriod(): Response
     {
         return $this->render('composants/button_period.html.twig');
