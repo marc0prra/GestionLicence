6e84e81cd1b27667bfe479ae17aeda30ae105973
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2026-01-05 11:28:18 +0100

    reference

--- config/reference.php
+++ config/reference.php
@@ -2,7 +2,7 @@
 
 // This file is auto-generated and is for apps only. Bundles SHOULD NOT rely on its content.
 
-// namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Symfony\Component\Config\Loader\ParamConfigurator as Param;
 
