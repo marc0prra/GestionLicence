65e0b2998bef339b12d6c87d3188ff9720b9a4a8
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2026-01-12 14:30:30 +0100

    avancé de course instructorfixtures

--- src/DataFixtures/CourseInstructorFixtures.php
+++ src/DataFixtures/CourseInstructorFixtures.php
@@ -3,17 +3,27 @@
 namespace App\DataFixtures;
 
 use App\Entity\CourseInstructor;
+use App\Entity\Instructor;
+use App\Entity\Course;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class CourseInstructorFixtures extends Fixture
+class CourseInstructorFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public function load(ObjectManager $manager): void
     {
-        $courseInstructor = new CourseInstructor();
-        $courseInstructor->$this->getReference(CourseFixtures);
+        $instructor = $this->getReference(InstructorFixtures::INSTRUCTOR_REFERENCE, Instructor::class);
 
+        $manager->persist($courseInstructor);
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            InstructorFixtures::class,
+            CourseFixtures::class,
+        ];
     }
 }
