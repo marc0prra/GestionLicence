6c28521d7a6ec94988eeed2155f3e48060c8bc22
Valentin <valentin.godard2006@gmail.com>
2026-01-19 09:27:35 +0100

    formulaire-Module

---
+++ src/Form/ModuleType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Module;
+use App\Entity\TeachingBlock;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
+
+class ModuleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $bloc = $options['bloc_actuel'];
+
+        $builder
+            ->add('bloc', EntityType::class, [
+                'class' => TeachingBlock::class,
+                'choice_label' => 'nom',
+                'label' => 'Bloc enseignement',
+                'disabled' => true,
+                'required' => true,
+            ])
+            ->add('code', TextType::class, [
+                'label' => 'Code',
+                'required' => true,
+                'help' => 'Champ obligatoire, doit être unique.'
+            ])
+
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('heures', IntegerType::class, [
+                'label' => 'Nombre d\'heures',
+                'required' => false,
+            ])
+
+            ->add('parent', EntityType::class, [
+                'class' => Module::class,
+                'label' => 'Parent',
+                'required' => false,
+                'placeholder' => 'Sélectionnez un parent (optionnel)',
+                'choice_label' => 'nom',
+                'query_builder' => function (EntityRepository $er) use ($bloc) {
+                    return $er->createQueryBuilder('m')
+                        ->where('m.bloc = :bloc')
+                        ->setParameter('bloc', $bloc)
+                        ->orderBy('m.nom', 'ASC');
+                },
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => true,
+                'attr' => ['rows' => 4],
+            ])
+            ->add('projetFilRouge', CheckboxType::class, [
+                'label' => 'Module effectué sur le projet fil rouge',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Module::class,
+            'bloc_actuel' => null, 
+        ]);
+
+        $resolver->setAllowedTypes('bloc_actuel', ['null', TeachingBlock::class]);
+    }
+}
