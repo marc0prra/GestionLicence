2e16f7272b97cd1404bc6727959d997463066692
marc0prra <pereira.marco2006@gmail.com>
2026-01-26 16:57:37 +0100

    Ajout du formulaire pour la création d'une nouvelle période de cours avec validation et styles améliorés

--- src/Controller/AnneesScolaireController.php
+++ src/Controller/AnneesScolaireController.php
@@ -7,6 +7,10 @@ use App\Repository\SchoolYearRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\CoursePeriod;
+use App\Form\CoursePeriodType;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class AnneesScolaireController extends AbstractController
 {
@@ -17,6 +21,28 @@ final class AnneesScolaireController extends AbstractController
 
         return $this->render('annees_scolaire/annees_scolaire.html.twig', [
             'dataAnneesScolaire' => $anneesScolaire
+        ]);
+    }
+
+    #[Route('/annees/scolaire/new', name: 'app_course_period_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $coursePeriod = new CoursePeriod();
+        $form = $this->createForm(CoursePeriodType::class, $coursePeriod);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($coursePeriod);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La promotion a bien été enregistrée.');
+
+            return $this->redirectToRoute('app_course_period_new', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('annees_scolaire/new_course_period.html.twig', [
+            'course_period' => $coursePeriod,
+            'form' => $form,
         ]);
     }
 }
