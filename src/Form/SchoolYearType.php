c3331180d73bb7dfd5b217c5d3136b7dfb84d83f
RedwaneK <redwanekouadria@gmail.com>
2026-05-11 11:23:40 +0200

    Test

--- src/Form/SchoolYearType.php
+++ src/Form/SchoolYearType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
+
 class SchoolYearType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
